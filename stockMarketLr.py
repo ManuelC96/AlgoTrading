647be01e3e642ff1c6adfdd90b89a434434ba3a3
ManuelC96 <manuelchiocchetta@gmail.com>
2023-10-06 17:22:37 +0200

    First commit

--- stockMarketLr.py
+++ stockMarketLr.py
@@ -2,17 +2,52 @@
 import yfinance as yf
 import numpy as np
 import pandas as pd 
-from matplotlib import pyplot as pyplot
 import requests
+import os
+from bs4 import BeautifulSoup as bs
+from matplotlib import pyplot as pyplot
+from dotenv import load_dotenv
+
+# env management
+load_dotenv()#take environment variables from .env.
+url = os.getenv('S&P500list')
 
 
-# nasdaq api
-link = "https://data.nasdaq.com/api/v3/datasets/"
-apiKey = "?api_key=dxacqqyjomm6cyJhG_VZ"
-endpoint = "MER/F1.xml"
-finalUrl = link + endpoint + apiKey
-print(finalUrl)
-# retrive stock data
-data = requests.get(url="https://data.nasdaq.com/api/v3/datatables/ETFG/FUND.csv?ticker=SPY&api_key=dxacqqyjomm6cyJhG_VZ")
-print(data.content)
-# data = yf.Ticker()
+# retrive S&P 500 company tickers
+rawPage = requests.get(url)
+soup = bs(rawPage.text, "html.parser")
+simbolRows = soup.find_all("a", attrs={"rel":"nofollow","class":"external text" } )
+simbols = tuple()
+for i in range(len(simbolRows)):
+    if len(simbolRows[i].text.strip()) < 5:
+        simbols += (simbolRows[i].text.strip(),)
+    else:
+        pass
+
+nameRows = soup.find_all("tr")
+for i in nameRows:
+    nameRows += i.find_all("td")
+
+equityNames = list()
+for j in range(len(nameRows)):
+    equityName += [nameRows[j].text.split('\n')]
+
+equityNames.pop(0)
+
+companies = list()
+for k in range(503):
+    print(f'{k} ', equityNames[k], '\n', len(equityNames[k]) )
+    if len(equityNames[k]) >= 8:
+        companies.append(equityNames[k][3]) 
+    else:
+        pass
+
+
+
+
+# get historycal financial data
+
+# tk = symbol
+
+
+
