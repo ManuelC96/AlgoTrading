883b87c2fc93bb2866605970ad309f8864d7e7df
Artefici <arteficidesign@gmail.com>
2023-10-09 19:22:26 +0200

    First commit

--- stockMarketLr.py
+++ stockMarketLr.py
@@ -1,7 +1,7 @@
 # import modules
 import yfinance as yf
 import numpy as np
-import pandas as pd 
+import pandas as pd
 import requests
 import os
 from bs4 import BeautifulSoup as bs
@@ -9,20 +9,23 @@ from matplotlib import pyplot as pyplot
 from dotenv import load_dotenv
 
 # env management
-load_dotenv()#take environment variables from .env.
+load_dotenv()  # take environment variables from .env.
 url = os.getenv('S&P500list')
 
 
 # retrive S&P 500 company tickers
 rawPage = requests.get(url)
 soup = bs(rawPage.text, "html.parser")
-simbolRows = soup.find_all("a", attrs={"rel":"nofollow","class":"external text" } )
-simbols = tuple()
-for i in range(len(simbolRows)):
-    if len(simbolRows[i].text.strip()) < 5:
-        simbols += (simbolRows[i].text.strip(),)
-    else:
-        pass
+
+# OLD code
+# simbolRows = soup.find_all(
+#     "a", attrs={"rel": "nofollow", "class": "external text"})
+# simbols = tuple()
+# for i in range(len(simbolRows)):
+#     if len(simbolRows[i].text.strip()) < 5:
+#         simbols += (simbolRows[i].text.strip(),)
+#     else:
+#         pass
 
 nameRows = soup.find_all("tr")
 for i in nameRows:
@@ -30,24 +33,55 @@ for i in nameRows:
 
 equityNames = list()
 for j in range(len(nameRows)):
-    equityName += [nameRows[j].text.split('\n')]
+    equityNames += [nameRows[j].text.split('\n')]
 
 equityNames.pop(0)
 
-companies = list()
+companies = dict()
 for k in range(503):
-    print(f'{k} ', equityNames[k], '\n', len(equityNames[k]) )
+    # print(f'{k} ', equityNames[k], '\n', len(equityNames[k]))
     if len(equityNames[k]) >= 8:
-        companies.append(equityNames[k][3]) 
+        companies[equityNames[k][3]] = equityNames[k][1]
     else:
         pass
 
 
-
-
 # get historycal financial data
+def getData(originalFunc):
+    def wrapperFunc(*args, **kwargs):
+        tk = yf.Ticker(originalFunc(*args, **kwargs))
+        return tk.history(period="1d", interval="1h")
+    return wrapperFunc
 
-# tk = symbol
+
+# retrive S&P 500 company tickers
+@getData
+def getSymbol(companyName: str):
+    # env management
+    load_dotenv()  # take environment variables from .env.
+    url = os.getenv('S&P500list')
+
+    # retrive S&P 500 company tickers
+    rawPage = requests.get(url)
+    soup = bs(rawPage.text, "html.parser")
+
+    nameRows = soup.find_all("tr")
+    for i in nameRows:
+        nameRows += i.find_all("td")
+
+    equityNames = list()
+    for j in range(len(nameRows)):
+        equityNames += [nameRows[j].text.split('\n')]
+
+    equityNames.pop(0)
+
+    companies = dict()
+    for k in range(503):
+        if len(equityNames[k]) >= 8:
+            companies[equityNames[k][3]] = equityNames[k][1]
+        else:
+            pass
+    return companies[companyName]
 
 
-
+df3M = getSymbol("3M")
